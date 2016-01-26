0a68223085dcc77c8a5e67fcd3d71e5ff87195b2
Gina Maini <gina@beancode.io>
2016-01-26 00:13:14 -0800

    FIX - add ocamlopt with correct path + remove README markdown

--- packages/ocaml-xarm.4.02.0/files/install.sh
+++ packages/ocaml-xarm.4.02.0/files/install.sh
@@ -6,6 +6,8 @@ rm -rf "${PREFIX}"/bin "${PREFIX}"/lib "${PREFIX}"/man
 
 sed -i bak -e 's|OCAMLDIR|'${PREFIX}'|g' ocamloptrev
 
+sed -i junk -e '1 s|^.*$|#!'${PREFIX}'/bin/ocamlrun|g' bin/ocamlopt
+
 mv bin "${PREFIX}"
 cp ocamloptrev "${PREFIX}"/bin
 # cp wrapped "${PREFIX}"/bin/ocamlopt
